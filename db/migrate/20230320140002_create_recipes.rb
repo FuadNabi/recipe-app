211d73b3c11b4063c4c7fe50ee1bde5b6f8116ad
jorgegoco <jorgegoco70@gmail.com>
2023-03-20 15:46:59 +0100

    Create the Food table

--- db/migrate/20230320140002_create_recipes.rb
+++ db/migrate/20230320140002_create_recipes.rb
@@ -5,7 +5,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
       t.string :preparation_time
       t.string :cooking_time
       t.text :description
-      t.boolean :public
+      t.boolean :public, default: true
 
       t.timestamps
 
