c18fd5b09e8b001e37a2c96b01bf80c9cef42c70
jorgegoco <jorgegoco70@gmail.com>
2023-03-21 13:27:16 +0100

    Add foods controller for current user

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -18,6 +18,12 @@ class FoodsController < ApplicationController
     end
   end
 
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
   private
 
   def food_params
