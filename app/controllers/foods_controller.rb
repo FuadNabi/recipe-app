12f355f30d9aef7c5042560199306c400b0f0f6a
jorgegoco <jorgegoco70@gmail.com>
2023-03-21 12:29:08 +0100

    Add new and create functionalities for Foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,26 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @foods = Food.where(user: current_user) || []
+  end
+
+  def new
+    @food = Food.new
+    render 'new'
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
