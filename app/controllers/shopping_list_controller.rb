68ceba12c2a756d88a2bcaf7651dad90ddb90fcf
jorgegoco <jorgegoco70@gmail.com>
2023-03-22 22:20:28 +0100

    Fix rubocop offenses

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -3,16 +3,14 @@ class ShoppingListController < ApplicationController
     @food_amount = 0
     @total_price = 0
     @recipe_foods = []
-    @need_foods = RecipeFood.where(recipe_id: params[:recipe_id] )
+    @need_foods = RecipeFood.where(recipe_id: params[:recipe_id])
     @need_foods.each do |need_food|
       new_food_id = need_food.food_id
       new_val = Food.where(user: current_user).where(id: new_food_id).first.quantity
       quantity_needed = new_val - need_food.quantity
-      if quantity_needed >= 0
-        next
-      else
-        need_food.quantity = quantity_needed * -1
-      end
+      next if quantity_needed >= 0
+
+      need_food.quantity = quantity_needed * -1
       @recipe_foods << need_food
     end
     @recipe_foods.each do |f|
