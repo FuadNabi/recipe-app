0cee8367f6793eb0abb1a38f21ae056e7254fcf6
FuadNabi <fuad.nebiyev95@mail.ru>
2023-03-23 16:50:04 +0400

    Fix lintern errors

--- spec/views/shopping_list/show.html.erb_spec.rb
+++ spec/views/shopping_list/show.html.erb_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Shopping list page view', type: :feature do
       @user = User.create!(email: 'max@email.com', password: 'password', name: 'Fuad')
       @recipe = Recipe.create!(user: @user, name: 'Ground beef', preparation_time: '10 min', cooking_time: '45 min',
                                description: 'Lorem ipsum', public: true)
-      @food = Food.create!(user_id: @user.id, name: 'Apple', measurement_unit: 'kg', price: 10)
+      @food = Food.create!(user_id: @user.id, name: 'Apple', measurement_unit: 'kg', price: 10, quantity: 1)
       @ingredient = RecipeFood.create!(quantity: 2, recipe_id: @recipe.id, food_id: @food.id)
 
       visit 'users/sign_in'
